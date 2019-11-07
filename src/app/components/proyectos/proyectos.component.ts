347cd1493d007dc07f49bcf58d038ae359c5da9b
Diego <diegomolina1983@gmail.com>
2019-11-07 15:29:49 -0300

    creacion del formulario de contacto

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProyectosService, Proyecto } from '../../services/proyectos.service';
 import { Router } from '@angular/router';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-proyectos',
@@ -10,12 +11,17 @@ import { Router } from '@angular/router';
 export class ProyectosComponent implements OnInit {
 
   proyectos:Proyecto[] = [];
+  loading:boolean;
 
   constructor( private _proyectosService:ProyectosService,
-               private ruter:Router ) { }
+               private ruter:Router ) {
+
+    this.loading = true;
+  }
 
   ngOnInit() {
     this.proyectos = this._proyectosService.getProyectos();
+    this.loading = false;
   }
 
   verProyecto(idx:number){
